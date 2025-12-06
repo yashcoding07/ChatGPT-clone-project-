acfe7a568ea0eed310638ea2bf3796ae0f4f5bb5
yashcoding07 <yash07coding@gmail.com>
2025-12-06 12:31:35 +0530

    feat: add theme toggle functionality and update styling for login and register pages

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const cors = require('cors');
 const cookieParser = require("cookie-parser");
 /*Routes */
 const authRoutes = require("./routes/Auth.routes");
 const chatRoutes = require("./routes/chat.routes");
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
--- Frontend/src/Pages/Register.jsx
+++ Frontend/src/Pages/Register.jsx
@@ -1,39 +1,84 @@
-import React from "react";
-import "../Styles/register.css"
+import "../styles/register.css";
+import ThemeToggle from "../Components/Themetoggle";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
+
+  const [form, setform] = useState({email: "", firstname: "", lastname: "", password: ""});
+  const navigate = useNavigate();
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setform((prev) => ({ ...prev, [name]: value }));
+  }
+
+  async function handleSubmit(e){
+      e.preventDefault();
+      axios.post("http://localhost:3000/api/auth/register", {
+        email: form.email,
+        fullName: {
+          firstName: form.firstname,
+          lastName: form.lastname,
+        },
+        password: form.password
+      },{
+        withCredentials: true
+      }).then((res)=>{
+        console.log(res)
+        navigate("/");
+      }).catch((err)=>{
+        console.error(err);
+      })
+  }
+
   return (
-    <div className="register-page">
-      <h1>Create Account</h1>
+    <>
+      <ThemeToggle />
+      <div className="auth-container">
+        <div className="register-title">
+          <h1>Create Account</h1>
+          <span>Join and explore AI-powered conversations</span>
+        </div>
 
-      <form className="register-form">
-        <input
-          type="text"
-          placeholder="Enter your first name"
-          className="input-field"
-        />
-        <input
-          type="text"
-          placeholder="Enter your last name"
-          className="input-field"
-        />
-        <input
-          type="email"
-          placeholder="Enter your email"
-          className="input-field"
-        />
-        <input
-          type="password"
-          placeholder="Create a password"
-          className="input-field"
-        />
-        <button className="btn">Register</button>
-      </form>
+        <form className="register-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="firstname"
+            placeholder="first Name"
+            className="input-field"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="lastname"
+            placeholder="Last Name"
+            className="input-field"
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="input-field"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="input-field"
+            onChange={handleChange}
+          />
 
-      <p>
-        Already have an account? <a href="/login">Login</a>
-      </p>
-    </div>
+          <button className="btn">Register</button>
+        </form>
+
+        <p className="auth-footer">
+          Already registered? <Link to={"/login"}>Login here</Link>
+        </p>
+      </div>
+    </>
   );
 };
 
